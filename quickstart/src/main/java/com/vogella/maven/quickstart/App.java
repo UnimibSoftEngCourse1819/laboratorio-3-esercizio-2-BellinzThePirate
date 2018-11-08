0bf27f6f493f53e8a22cc3a6a88a9dc3019edbdc
a.bellinzoni@campus.unimib.it <a.bellinzoni@campus.unimib.it>
2018-11-08 10:19:00 +0100

    maybe will work

---
+++ quickstart/src/main/java/com/vogella/maven/quickstart/App.java
@@ -0,0 +1,14 @@
+package com.vogella.maven.quickstart;
+import com.proofpoint.json.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+      
+    	System.out.println( "Hello crazy World!" );
+    }
+}
